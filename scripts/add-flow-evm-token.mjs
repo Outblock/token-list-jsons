80fb68600e4fac9be459d313bcddd590f2df39a7
Caos <caosbad@gmail.com>
2025-02-08 09:59:15 +0800

    add USDF and stgUSDT

--- scripts/add-flow-evm-token.mjs
+++ scripts/add-flow-evm-token.mjs
@@ -1,45 +1,41 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import fs from "fs";
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fs from 'fs'
 import axios from 'axios'
 import path from 'path'
-
 
 const args = process.argv
 const tokenAddress = args[2]
 const network = args[3]
 const identifier = args[4]
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const fixesEndpoints = {
-  mainnet: "https://raw.githubusercontent.com/fixes-world/token-list-jsons/refs/heads/main/jsons/mainnet",
-  testnet: "https://raw.githubusercontent.com/fixes-world/token-list-jsons/refs/heads/main/jsons/testnet"
-};
-
+  mainnet:
+    'https://raw.githubusercontent.com/fixes-world/token-list-jsons/refs/heads/main/jsons/mainnet',
+  testnet:
+    'https://raw.githubusercontent.com/fixes-world/token-list-jsons/refs/heads/main/jsons/testnet',
+}
 
 const queryTokenList = async (network) => {
   try {
     const url = `${fixesEndpoints[network]}/evm/default.json`
-    const response = await axios.get(url);
-    const data = await response.data;
+    const response = await axios.get(url)
+    const data = await response.data
     const { tokens } = data
-    if (
-      Array.isArray(tokens) &&
-      tokens.length > 0
-    ) {
+    if (Array.isArray(tokens) && tokens.length > 0) {
       return tokens
     } else {
       return []
     }
   } catch (e) {
     console.log(e)
-    console.error(`Failed to query default token list for ${network}`);
-    return [];
+    console.error(`Failed to query default token list for ${network}`)
+    return []
   }
-};
+}
 
 const loadOutblocTokenList = async (filePath) => {
   try {
@@ -48,16 +44,14 @@ const loadOutblocTokenList = async (filePath) => {
     const content = fs.readFileSync(resolvedPath, 'utf8')
 
     return JSON.parse(content)
-
   } catch (e) {
     console.log(e)
-    console.error(`Failed to load JSON from ${filePath}`);
-    return [];
+    console.error(`Failed to load JSON from ${filePath}`)
+    return []
   }
-};
+}
 
 const main = async () => {
-
   const tokenList = await queryTokenList(network)
 
   const tokenMaps = {}
@@ -73,43 +67,49 @@ const main = async () => {
   if (tokenMaps[tokenIden] !== undefined) {
     tokenInfo = tokenMaps[tokenIden]
   } else {
-    throw ("Token not found")
+    throw 'Token not found'
   }
 
-  const currentTokens = await loadOutblocTokenList(`../jsons/${network}/evm/default.json`)
+  const currentTokens = await loadOutblocTokenList(
+    `../jsons/${network}/evm/default.json`,
+  )
 
   const { tokens: outblockTokens } = currentTokens
 
-
   outblockTokens.map((outblockToken) => {
-
     const { address } = outblockToken
 
     if (`${address}` === tokenIden) {
-      throw ('Token already exist')
+      throw 'Token already exist'
     }
   })
 
   // evm token struct
   const newToken = {
-    "chainId": tokenInfo.chainId,
-    "address": tokenInfo.address,
-    "symbol": tokenInfo.symbol,
-    "name": tokenInfo.name,
-    "decimals": tokenInfo.decimals,
-    "logoURI": tokenInfo.logoURI,
-    "flowIdentifier": tokenInfo.flowIdentifier || identifier,
-    "tags": tokenInfo.tags,
-    "extensions": tokenInfo.extensions
+    chainId: tokenInfo.chainId,
+    address: tokenInfo.address,
+    symbol: tokenInfo.symbol,
+    name: tokenInfo.name,
+    decimals: tokenInfo.decimals,
+    logoURI: tokenInfo.logoURI,
+    flowIdentifier: tokenInfo.flowIdentifier || identifier,
+    tags: tokenInfo.tags,
+    extensions: {
+      website: tokenInfo.extensions
+        ? tokenInfo.extensions.website
+        : tokenInfo.website || '',
+    },
   }
 
   currentTokens.tokens.push(newToken)
   currentTokens.totalAmount = Number(currentTokens.totalAmount) + 1
 
-  fs.writeFileSync(path.resolve(__dirname, `../jsons/${network}/evm/default.json`), JSON.stringify(currentTokens, null, 2))
+  fs.writeFileSync(
+    path.resolve(__dirname, `../jsons/${network}/evm/default.json`),
+    JSON.stringify(currentTokens, null, 2),
+  )
 
   console.log(`token with address ${tokenAddress} add success`)
 }
 
-
-main();
+main()
